655f201d646413d06ee1ece0ae091ab73df99655
sakshi <sakshianandk@gmail.com>
2021-08-14 15:19:06 +0530

    basic toto app still has some errors

--- src/MyToDoList.js
+++ src/MyToDoList.js
@@ -1,52 +1,46 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 function MyToDoList() {
-    const [itemName,setItemName] = useState("");
-    const [lists,setLists] = useState([]);
+    const [events, setEvents] = useState("");
+    const [list, setList] = useState([]);
+    const deleteEvent = (id) => {
+        let data = list.filter((x) => x.id != id)
+        setList(data);
+    }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();//use to prevent default behaviour of our browser
-        if (itemName) {
-            // console.log('submit');
-            const items =  {id:new Date().getTime().toString(),itemName};
-            console.log(items);
-           
-            setLists((items) =>{
-                return[...lists,items]
+    const handleEvent = (e) => {
+        e.preventDefault();
+        if (events) {
+            console.log(events);
+
+            const items = { id:new Date().getTime().toString(),events };
+
+            setList((list) => {
+                return [...list, items]
             });
-            setLists('');
-        }else{
-            console.log('empty');
+            // setList(''); //getting an error after writing this
+        } else {
+            console.log("entervalue");
         }
-         console.log(itemName);
     }
     return (
         <>
-        <article>
-            <form className='form' onSubmit={handleSubmit}>
-
-            <div className='form-control'>
-                <label htmlFor="itemName"> Name :</label>
-                <input type="text" id="itemName" name="itemName" value={itemName} onChange={(e) => setItemName(e.target.value)}></input>
-            </div>
-
-          
-
-            <button type="submit">Add</button>
-            </form>
-            {
-                lists.map((items,index) =>{
-                    const {id,itemName} = items;
-                    return(
-                        <div className="item" key={id}>
-                            <h4>{itemName}</h4>
-                           
+            <article>
+                <form className="form" onSubmit={handleEvent}>
+                    <div className="form-control">
+                        <label>Add Item :</label>
+                        <input type="text" value={events} onChange={(e) => setEvents(e.target.value)}></input>
+                    </div>
+                    <button type="submit">Add Item</button>
+                </form>
+                {list.map((items) => {
+                    return (
+                        <div key={items.id} className="item">{items.events}
+                            <button onClick={() => { deleteEvent(items.id) }}>delete</button>
                         </div>
                     )
-                })
-            }
-        </article>
-
+                })}
+            </article>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>My ToDo App</h1>
+    <hr></hr>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
